bc852566cfbb078b8f7626f6c30f6e28806ba7e0
Ryan Bruschi <ryan.bruschi@gmail.com>
2023-10-31 18:54:54 +1000

    First API Array Store
    Updated api to call all str wars chjaracters and then print the array

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 
 import requests
-api_url = "https://swapi.dev/api/people/1"
-response = requests.get(api_url)
-response.json()
-print(response)
-print(response.json())
+# swapi, has all character info
+url = "https://swapi.dev/api/people/"
+# response = requests.get(api_url)
+# jsonwebdata = response.json()
+# print(response)
+# print(response.json())
+
+# store the starwars api data
+character_data = []
+
+while url:
+    # Send an HTTP GET request to the API
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+
+        # Append the results (people) to the all_people_data list
+        character_data.extend(data.get('results', []))
+
+        # Check if there's a next page
+        url = data.get('next')
+    else:
+        # If the request was not successful, print an error message
+        print(f"Failed to retrieve data. Status code: {response.status_code}")
+        break
+
+# Now, all_people_data contains data for all people
+for person in character_data:
+    print("Name: ", person['name'])
+    print("Height: ", person['height'])
+    print("Mass: ", person['mass'])
+    print("Hair Color: ", person['hair_color'])
